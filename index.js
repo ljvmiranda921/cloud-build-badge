dc2a761cb0e3033529adc60914da21e42c7341e4
sbsends <sbsends@gmail.com>
2018-11-15 23:58:39 -0600

    Add features for v1.0.0

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * gcloud functions deploy <FUNC_NAME> --runtime nodejs6 --trigger-resource cloud-builds --trigger-event google.pubsub.topic.publish
+ * @param {object} event The Cloud Functions event.
+ * @param {function} callback The callback function.
+ */
+const { Storage } = require('@google-cloud/storage');
+
+exports.${badge} = (event, callback) => {
+  const pubsubMessage = event.data;
+  if  (pubsubMessage.data) {
+    buildResource = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
+    if (buildResource.source.repoSource.repoName && buildResource.source.repoSource.branchName && buildResource.status) {
+      repo = buildResource.source.repoSource.repoName === "${repo}";
+      branch = buildResource.source.repoSource.branchName === "${branch}";
+      status = buildResource.status;
+
+      const storage = new Storage();
+      if (repo && branch && status == "SUCCESS") {
+        storage.bucket("${bucket}")
+        .file("build/success.svg")
+        .copy(storage.bucket("${bucket}")
+        .file("build/${badge}.svg"));
+        console.log("switched badge to build success")
+      }
+      if (repo && branch && status == "FAILURE") {
+        storage.bucket("${bucket}")
+        .file("build/failure.svg")
+        .copy(storage.bucket("${bucket}")
+        .file("build/${badge}.svg"));
+        console.log("switched badge to build failure")
+      }
+    }
+  }
+  callback();
+};
+
+
+
