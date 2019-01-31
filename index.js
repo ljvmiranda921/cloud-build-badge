96c5bc5caa3d5295b3616db350b457b486107f38
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-01-31 14:54:43 +0800

    Update comments

--- index.js
+++ index.js
@@ -6,43 +6,41 @@ const { renderString, renderTemplateFile } = require("template-file");
 
 var utils = require("./utils");
 
-/**
- * Build command-line arguments
- */
+// Build command-line arguments
 const argv = require("yargs")
-  .options("id", {
+.usage("Create a deployable Cloud Function by providing arguments to the parameters below")
+.options("id", {
     alias: "i",
-    describe: "deploy function unique ID",
-    demand: "function ID must be specified",
+    describe: "Deploy function unique ID",
+    demand: "Function ID must be specified",
     type: "string"
   })
   .options("repository", {
     alias: "r",
-    describe: "target repository",
-    demand: "target repository must be specified",
+    describe: "Target repository",
+    demand: "Target repository must be specified",
     type: "string"
   })
   .options("branches", {
     alias: "b",
-    describe: "target branches",
-    demand: "a branch or list of branches must be specified",
+    describe: "Target branches",
+    demand: "A branch or list of branches must be specified",
     type: "array"
   })
   .options("bucket", {
     alias: "k",
-    describe: "bucket to source and store badges",
-    demand: "bucket must be specified",
+    describe: "Bucket to source and store badges",
+    demand: "Bucket must be specified",
     type: "string"
   })
   .options("verbose", {
     alias: "v",
     default: false
   })
+  .epilogue("For more information, please visit: https://github.com/ljvmiranda921/cloud-build-badge")
   .help().argv;
 
-/**
- * Setup data for template
- */
+// Setup data for template
 const data = {
   id: argv.id,
   repository: argv.repository,
@@ -50,10 +48,10 @@ const data = {
   bucket: argv.bucket
 };
 
-/**
- * Render template and save to file
- */
+// Render template and save to file
 console.log(chalk.yellow("Creating a deploy function from template"));
 renderTemplateFile(path.resolve(__dirname, "./templates/template-github"), data)
-  .then(renderedString => utils.renderFile(renderedString, "function.js", argv.id))
+  .then(renderedString =>
+    utils.renderFile(renderedString, "function.js", argv.id)
+  )
   .then(renderedString => utils.printVerbose(renderedString, argv.verbose));
